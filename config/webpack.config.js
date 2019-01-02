295dd3be200c07c0d70315658c640709b85249e2
gepengcheng <gpc_1994@163.com>
2019-01-02 15:24:32 +0800

    2019/1/2

---
+++ config/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+const config = {
+  mode: 'none',
+  entry: {
+    main: path.join(__dirname, '../src/main.js')
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.join(__dirname, '../dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(
+      ['dist'],
+      {
+        root: path.join(__dirname, '../')
+      }
+    ),
+    new HtmlWebpackPlugin({
+      title: 'webpack demo',
+      template: path.join(__dirname, '../index.html'),
+      inject: true,
+      minify: {
+        removeComments: true
+      }
+    })
+  ]
+}
+
+module.exports = config
